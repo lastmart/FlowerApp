f7e070f2c03d54039601f3b8916914a779ffd5c6
Alexey <greenbelg@internet.ru>
2025-01-07 19:41:14 +0500

    Create SurveyFlower

---
+++ FlowerApp.Data/Configurations/SurveyFlowerConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using FlowerApp.Data.DbModels.Surveys;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FlowerApp.Data.Configurations;
+
+public class SurveyFlowerConfiguration : IEntityTypeConfiguration<SurveyFlower>
+{
+    public void Configure(EntityTypeBuilder<SurveyFlower> builder)
+    {
+        builder.HasKey(flower => flower.Id);
+
+        builder.Property(flower => flower.RelevantVariants)
+            .IsRequired();
+
+        // TODO add constraints
+    }
+}

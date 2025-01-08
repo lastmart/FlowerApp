323f9155a4fd48915976919266d938696f8724aa
Alexey <greenbelg@internet.ru>
2025-01-08 17:25:02 +0500

    Merge branch 'Add_Survey_Flower'

---
+++ FlowerApp.Data/DbModels/Surveys/SurveyFlower.cs
@@ -0,0 +1,13 @@
+﻿using FlowerApp.Data.DbModels.Flowers;
+
+namespace FlowerApp.Data.DbModels.Surveys;
+
+public class SurveyFlower : Entity<int>
+{
+    public string RelevantVariants { get; set; }
+    public int FlowerId { get; set; }
+    public int SurveyQuestionId { get; set; }
+
+    public Flower Flower { get; set; }
+    public SurveyQuestion SurveyQuestion { get; set; }
+}

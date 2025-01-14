8b4ae2c5e74b089d51ddeb31def628df4e3eb971
Alexey <greenbelg@internet.ru>
2025-01-14 16:07:10 +0500

    Change settings

--- FlowerApp.Service/Configuration/ApiSettings.cs
+++ FlowerApp.Service/Configuration/ApiSettings.cs
@@ -4,8 +4,6 @@ public class ApiSettings
 {
     public string RecommendationSystemUrl { get; init; } = "http://127.0.0.1:8000/recommendations";
     public string GoogleAuthUrl { get; init; } = "https://oauth2.googleapis.com/token";
-    // Кароч это поле нужно, если у нас есть web версия приложения. Если появится - поймем зачем.
-    // У нас такого нет, поэтому стандарт указывает проставлять пустую строку
     public string RedirectedUrl { get; init; } = "";
     public string GoogleUserInfoUrl { get; init; } = "https://www.googleapis.com/oauth2/v3/userinfo";
     public string GoogleTokenInfoUrl { get; init; } = "https://oauth2.googleapis.com/tokeninfo";
